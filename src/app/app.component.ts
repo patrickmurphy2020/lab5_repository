a205515909bb95dfbd8fb22c8ae583d80c90eb75
Patrick <g00379725@gmit.ie>
2021-02-24 15:17:06 +0000

    Lab Complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'root-directive',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'my-first-app';
+
+  Name: string = "Patrick";
+  Age: number = 19;
 }
---
+++ src/app/componentb/componentb.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-componentb',
+  templateUrl: './componentb.component.html',
+  styleUrls: ['./componentb.component.scss']
+})
+export class ComponentbComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
